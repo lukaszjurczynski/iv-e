a613c837a962bff76fe7ee1d8413b587a62e4c8a
Lukasz Jurczynski <l.jurczynski@gmail.com>
2021-12-15 08:06:15 +0100

    Add functionality to get data from API and render it on the page

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const containerEL = document.querySelector('.container');
+
+//Render items
+function createItemHTML(e) {
+    e.forEach(element => {
+        containerEL.insertAdjacentHTML('beforeend', (
+            `
+        <figure class="item">
+            <h3 class="description">
+                ${element.alt_description}
+            </h3>
+            <div class="item__text-content">
+                <div class="item__text-content--details">
+                    <p class="Author"><span class="indicator">By:</span><a href="${element.links.html}" class="link" target="self">${element.user.name}</a></p>
+                    <p class="date"><span class="indicator">On:</span>${element.created_at}</p>
+                </div>
+            </div>
+            <img src="${element.urls.small}" alt="image" class="item__background-image">
+        </figure>
+        `
+        ))
+    });
+}
+
+//Get data from user
+const createData = (word) => {
+    word = prompt('What images do you want to see?');
+    const promiseResult = fetch(`https://api.unsplash.com/search/photos?client_id=1TVmlW6RlL7ed_OZtwhC9zmr4JyI-rBmLQPjn6O4aDU&orientation=portrait&page=1&query=${word}&per_page=8`).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        createItemHTML(data.results)
+    });
+}
+
+createData();
